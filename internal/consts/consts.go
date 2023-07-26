a657822c30976fa11f876c298593174595e55fe0
zhiqiang.xu <1004971804@qq.com>
2023-07-26 17:08:18 +0800

    fix:代码结构调整

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,8 +1,5 @@
 package consts
 
 var (
-	ProjectName     = "基于GoFrame2搭建的Web项目脚手架"
-	ProjectUsage    = "Golang项目开发脚手架"
-	ProjectBrief    = "start http server"
 	ShowTableStatus = "SHOW TABLE STATUS"
 )
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	_ "context"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+
 	"github.com/Jiaru0314/go_gen_code/codeGenUtil"
 	_ "github.com/Jiaru0314/go_gen_code/gendao"
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 )
 
 func main() {
